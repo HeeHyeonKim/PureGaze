3f3cfe3590bb3c4fae3a4d18ea0d5f111609994b
HEEHYEON <55612326+HeeHyeonKim@users.noreply.github.com>
2022-02-03 16:00:51 +0900

    시선 추정 metric 확인
    2D 시선 벡터를 사용하던 기존의 방식과 다르게 PureGaze에서는 2D 시선 벡터를 3D로 변환하여 각도를 연산한다.

--- model/gtools.py
+++ model/gtools.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+# 모델 클래스의 출력인 theta와 pi를 기반으로 3차원 시선 벡터를 추출한다.
 def gazeto3d(gaze):
   assert gaze.size == 2, "The size of gaze must be 2"
   gaze_gt = np.zeros([3])
@@ -8,7 +9,7 @@ def gazeto3d(gaze):
   gaze_gt[2] = -np.cos(gaze[1]) * np.cos(gaze[0])
   return gaze_gt
 
-
+# 모델 클래스의 출력을 변환한 3차원 시선 벡터와 GT를 통해 시선의 오차를 추출한다.
 def angular(gaze, label):
   assert gaze.size == 3, "The size of gaze must be 3"
   assert label.size == 3, "The size of label must be 3"
